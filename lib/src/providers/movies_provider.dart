7b61e26ef6604c069dfc54a9255e549cc8d4aa60
Christian Mena <christian.91.gm@hotmail.com>
2020-11-18 17:25:38 -0500

    fixed error get data api

--- lib/src/providers/movies_provider.dart
+++ lib/src/providers/movies_provider.dart
@@ -3,23 +3,19 @@ import 'package:http/http.dart' as http;
 import 'package:movies/src/config/config.dart';
 import 'package:movies/src/models/movie_model.dart';
 
-
 class MoviesProvider {
-
   Future<List<Movie>> getOnCinema() async {
-
-    final url = Uri.https(urlApi, '3/movie/now_playing',{
-      'api-key':'1cb0d9b4dd98d6250b0dffcca928faf1',
-      'language':language,
+    final url = Uri.https(urlApi, '3/movie/now_playing', {
+      'api_key': apiKey,
+      'language': language,
     });
 
     final resp = await http.get(url);
 
     final decodedData = json.decode(resp.body);
 
-    print(url);
+    print(decodedData);
 
     return [];
-
   }
 }
