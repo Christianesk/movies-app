cefed4e6539c14277343806c1d2814441735c53a
Christian Mena <christian.91.gm@hotmail.com>
2020-11-23 15:50:44 -0500

    Using streams

--- lib/src/providers/movies_provider.dart
+++ lib/src/providers/movies_provider.dart
@@ -1,9 +1,25 @@
+import 'dart:async';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:movies/src/config/config.dart';
 import 'package:movies/src/models/movie_model.dart';
 
 class MoviesProvider {
+
+  int _popularPage = 0;
+
+  List<Movie> _popularMovies = new List();
+
+  final _popularStreamController = new StreamController<List<Movie>>.broadcast();
+
+  Function(List<Movie>) get popularSink => _popularStreamController.sink.add;
+
+  Stream<List<Movie>> get popularStream => _popularStreamController.stream;
+
+  void disposeStreams(){
+    _popularStreamController?.close();
+  }
+
   Future<List<Movie>> getOnCinema() async {
     final url = Uri.https(urlApi, '3/movie/now_playing', {
       'api_key': apiKey,
@@ -14,12 +30,22 @@ class MoviesProvider {
   }
 
   Future<List<Movie>> getPopular() async {
+    _popularPage++;
+
     final url = Uri.https(urlApi, '3/movie/popular', {
-      'api_key': apiKey, 
-      'language': language
+      'api_key': apiKey,
+      'language': language,
+      'page': _popularPage.toString()
     });
 
-    return await _processResponse(url);
+    final resp = await _processResponse(url);
+
+
+    _popularMovies.addAll(resp);
+
+    popularSink(_popularMovies);
+
+    return resp;
   }
 
   Future<List<Movie>> _processResponse(Uri url) async {
