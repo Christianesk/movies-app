b3964c65702024779b02b9adc82162fdb8a0b79f
Christian Mena <christian.91.gm@hotmail.com>
2020-11-26 21:32:52 -0500

    View of actors per movie complete

--- lib/src/pages/movie_detail.dart
+++ lib/src/pages/movie_detail.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:movies/src/models/actors_model.dart';
 import 'package:movies/src/models/movie_model.dart';
+import 'package:movies/src/providers/actors_provider.dart';
 
 class MovieDetailPage extends StatelessWidget {
   @override
@@ -11,17 +13,16 @@ class MovieDetailPage extends StatelessWidget {
         slivers: <Widget>[
           _createAppBar(movie),
           SliverList(
-            delegate: SliverChildListDelegate(
-              [ 
-                SizedBox(height: 10.0),
-                _posterTitle(context,movie),
-                _description(movie),
-                _description(movie),
-                _description(movie),
-                _description(movie),
-                _description(movie)
-              ]
-            ),
+            delegate: SliverChildListDelegate([
+              SizedBox(height: 10.0),
+              _posterTitle(context, movie),
+              _description(movie),
+              _description(movie),
+              _description(movie),
+              _description(movie),
+              _description(movie),
+              _createCasting(movie)
+            ]),
           ),
         ],
       ),
@@ -68,12 +69,17 @@ class MovieDetailPage extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                Text(movie.title,style: Theme.of(context).textTheme.headline5, overflow: TextOverflow.ellipsis),
-                Text(movie.originalTitle,style: Theme.of(context).textTheme.subtitle1,overflow: TextOverflow.ellipsis),
+                Text(movie.title,
+                    style: Theme.of(context).textTheme.headline5,
+                    overflow: TextOverflow.ellipsis),
+                Text(movie.originalTitle,
+                    style: Theme.of(context).textTheme.subtitle1,
+                    overflow: TextOverflow.ellipsis),
                 Row(
                   children: [
                     Icon(Icons.star_border),
-                    Text(movie.voteAverage.toString(),style: Theme.of(context).textTheme.subtitle1)
+                    Text(movie.voteAverage.toString(),
+                        style: Theme.of(context).textTheme.subtitle1)
                   ],
                 )
               ],
@@ -86,10 +92,60 @@ class MovieDetailPage extends StatelessWidget {
 
   Widget _description(Movie movie) {
     return Container(
-      padding: EdgeInsets.symmetric(horizontal: 10.0,vertical: 20.0),
+      padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
       child: Text(
         movie.overview,
         textAlign: TextAlign.justify,
+      ),
+    );
+  }
+
+  Widget _createCasting(Movie movie) {
+    final actorProvider = new ActorsProvider();
+
+    return FutureBuilder(
+      future: actorProvider.getCast(movie.id.toString()),
+      builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
+        if (snapshot.hasData) {
+          return _createActorPageView(snapshot.data);
+        } else {
+          return Center(child: CircularProgressIndicator());
+        }
+      },
+    );
+  }
+
+  Widget _createActorPageView(List<Actor> actors) {
+    return SizedBox(
+      height: 200.0,
+      child: PageView.builder(
+        pageSnapping: false,
+        controller: PageController(viewportFraction: 0.3, initialPage: 1),
+        itemCount: actors.length,
+        itemBuilder: (context, i) => _cardActor(actors[i]),
+      ),
+    );
+  }
+
+  Widget _cardActor(Actor actor) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 2.0),
+      child: Column(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20.0),
+            child: FadeInImage(
+              image: NetworkImage(actor.getPhoto()),
+              placeholder: AssetImage('assets/img/no-image.jpg'),
+              height: 150.0,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Text(
+            actor.name,
+            overflow: TextOverflow.ellipsis,
+          )
+        ],
       ),
     );
   }
--- lib/src/providers/actors_provider.dart
+++ lib/src/providers/actors_provider.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:movies/src/config/config.dart';
@@ -6,7 +6,7 @@ import 'package:movies/src/models/actors_model.dart';
 
 class ActorsProvider {
   Future<List<Actor>> getCast(String movieId) async {
-    final url = Uri.https(urlApi, '/movie/$movieId/credits', {
+    final url = Uri.https(urlApi, '3/movie/$movieId/credits', {
       'api_key': apiKey,
       'language': language,
     });
